8d46644afac2dbaa28485b6207f184962c0f79a9
bijay135 <bijaya.shah@ip-192-168-1-159.ec2.internal>
2022-04-28 13:18:54 +0545

    added flag and input type paramaters to script

--- python-scripting/control.py
+++ python-scripting/control.py
@@ -1,5 +1,12 @@
-import sys
+import argparse 
 import subprocess
+
+# Definatons
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--mode", dest = "mode", help="Control mode start, stop, status")
+args = parser.parse_args()
+if args.mode != "status":
+    instance = input("Enter instance number(space seperated for multiple), all:  ")
 
 # Variables
 instance_pool = [1, 2, 3]
@@ -9,29 +16,29 @@ def start_instance(instance_number):
     subprocess.call("/Users/bijaya.shah/Applications/apache-tomcat-8.5.50/"+str(instance_number)+"/bin/startup.sh")
 
 def stop_instance(instance_number):
-    subprocess.call("/Users/bijaya.shah/Applications/apache-tomcat-8.5.50/{}/bin/shutdown.sh".format(instance_number))
+    subprocess.call("/Users/bijaya.shah/Applications/apache-tomcat-8.5.50/"+str(instance_number)+"/bin/shutdown.sh")
 
 def check_status():
     subprocess.run("lsof -i -n -P | grep '8001' | grep 'LISTEN'", shell=True)
     subprocess.run("lsof -i -n -P | grep '8002' | grep 'LISTEN'", shell=True)
     subprocess.run("lsof -i -n -P | grep '8003' | grep 'LISTEN'", shell=True)
 
-# Commands with paramaters
-if sys.argv[1] == "start":
-    if sys.argv[2] == "all":
+# Commands
+if args.mode == "start":
+    if instance == "all":
         for instance_number in instance_pool:
             start_instance(instance_number)
     else:
-        for instance_number in sys.argv[2:]:
+        for instance_number in instance.split():
             start_instance(instance_number)
-elif sys.argv[1] == "stop":
-    if sys.argv[2] == "all":
+elif args.mode == "stop":
+    if instance == "all":
         for instance_number in instance_pool:
             stop_instance(instance_number)
     else:
-        for instance_number in sys.argv[2:]:
+        for instance_number in instance.split():
             stop_instance(instance_number)  
-elif sys.argv[1] == "status":
+elif args.mode == "status":
     check_status()
 else:
     print("Command not supported. Check and try again")
